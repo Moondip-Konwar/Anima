ed55ebeb0fb7a6561126cb4de944258c7603583b
Moondip Konwar <moondipkonwar9@gmail.com>
2025-09-15 17:51:54 +0530

    Removed python calls

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,5 @@
 import { fetchBasicAnimeData, fetchPaginatedAnimeData } from "../lib/utils/fetch";
 
-import { invoke } from "@tauri-apps/api/core";
 export const load = async ({ setHeaders }) => {
 	const popularAnime = await fetchBasicAnimeData("POPULARITY_DESC");
 	const topRatedAnime = await fetchPaginatedAnimeData("SCORE_DESC", 1);
@@ -10,17 +9,6 @@ export const load = async ({ setHeaders }) => {
 	setHeaders({
 		"cache-control": "public, max-age=172800, stale-while-revalidate=86400"
 	});
-
-	async function call_python_script() {
-		const result = await invoke("run_python", {
-			script: "backend/test.py",
-			args: ["arg1", JSON.stringify({ apple: "banana", aa: 12312 })]
-		});
-		console.log("Python said:", result); // [Log] Python said: – "['arg1', '{\"apple\":\"banana\",\"aa\":12312}']" (+page.js, line 19)
-	}
-
-	call_python_script();
-	console.log("Called python script");
 
 	return {
 		popularAnime,
